9f264b1c66e0630ddb9675fec1e0562874aab70c
DanielHo-BS <danielho1671@gmail.com>
2022-03-24 10:06:51 +0800

    Add init-sdmmc (#1)
    * Add init-sdmmc
    
    * Minor update
    
    * Minor update

--- sdmmc/copy-rootfs-sdmmc.sh
+++ sdmmc/copy-rootfs-sdmmc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Mount the SDMMC as /mnt
+sudo umount /dev/mmcblk1p1
 sudo mkfs.ext4 /dev/mmcblk1p1
+
 sudo mount /dev/mmcblk1p1 /mnt
 # Copy over the rootfs from the eMMC to the SDMMC
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
